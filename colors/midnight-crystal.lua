4d74b0091246bbb400dc72e0f1ef589b95cc3d0e
Keith Henderson <keith.donaldh@gmail.com>
2026-01-27 20:22:58 -0500

    initial commit

---
+++ colors/midnight-crystal.lua
@@ -0,0 +1,8 @@
+-- ============================================================================
+-- Midnight Crystal Colorscheme Loader
+-- This file allows using: colorscheme midnight-crystal
+-- ============================================================================
+
+lua << EOF
+require('midnight-crystal').load()
+EOF
